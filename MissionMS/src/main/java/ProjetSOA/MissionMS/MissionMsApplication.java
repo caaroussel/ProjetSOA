4427f2082fb658cc13f1b4cc80459c9c8d822101
clementroussel <caa.roussel@gmail.com>
2023-12-27 17:26:52 +0100

    Ajout de MissionMS

---
+++ MissionMS/src/main/java/ProjetSOA/MissionMS/MissionMsApplication.java
@@ -0,0 +1,13 @@
+package ProjetSOA.MissionMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MissionMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MissionMsApplication.class, args);
+	}
+
+}
---
+++ MissionMS/src/test/java/ProjetSOA/MissionMS/MissionMsApplicationTests.java
@@ -0,0 +1,13 @@
+package ProjetSOA.MissionMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MissionMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
